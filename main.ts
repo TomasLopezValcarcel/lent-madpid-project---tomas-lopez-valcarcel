f834da76764f67e430c49f9a5a80eb97978b2237
TomasLopezValcarcel <63342663+TomasLopezValcarcel@users.noreply.github.com>
2021-02-11 16:23:14 +0000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,44 +1,110 @@
-tiles.setTilemap(tiles.createTilemap(hex`10001000020303030303030303030303030303060108080808080808080808080808080501080a0a0a0a0a080a0a080808080805010a0a080c0c0c0c0c0c090a09080805010a0c080a0a080a0a080c0c0c0a0805010a0c0c0a080a09090a0a0a0808080501080c0c0c0c0c090809080a0808080501080a09080a0a0a0a080a0a0a08080501080a09080a09080c0c08090a08080501080a09080a0809080908090a08080b01080a090808080c0808090908080c0501080809080808080a0a09080808080501080809090c0c0c0c090a0a0808080501080a0a0a0a0a0808080808080a0805010808080c0c0c0c0c0c0c0a0a0a0a050c0c0c0c040404040404040404040407`, img`
-    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    2 . . . . . . . . . . . . . . 2 
-    2 . . . . . . . . . . . . . . 2 
-    2 . . . 2 2 2 2 2 2 . . . . . 2 
-    2 . 2 . . . . . . . 2 2 2 . . 2 
-    2 . 2 2 . . . . . . . . . . . 2 
-    2 . 2 2 2 2 2 . . . . . . . . 2 
-    2 . . . . . . . . . . . . . . 2 
-    2 . . . . . . . 2 2 . . . . . 2 
-    2 . . . . . . . . . . . . . . . 
-    2 . . . . . . 2 . . . . . . 2 2 
-    2 . . . . . . . . . . . . . . 2 
-    2 . . . . 2 2 2 2 . . . . . . 2 
-    2 . . . . . . . . . . . . . . 2 
-    2 . . . 2 2 2 2 2 2 2 . . . . 2 
-    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    `, [myTiles.transparency16,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.darkGroundNorthEast1,sprites.dungeon.doorLockedEast,sprites.dungeon.floorLight0], TileScale.Sixteen))
-scene.centerCameraAt(0, 10000000)
-let mySprite = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-mySprite.setPosition(28, 70)
-mySprite.ay = 500
-controller.moveSprite(mySprite)
-scene.cameraFollowSprite(mySprite)
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.vy == 0) {
+        mySprite.vy = -160
+    }
+})
+function levelstart () {
+    scene.centerCameraAt(0, 10000000)
+    mySprite = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    if (level == 0) {
+        tiles.setTilemap(tilemap`level1`)
+    } else {
+    	
+    }
+    controller.moveSprite(mySprite, 100, 0)
+    scene.cameraFollowSprite(mySprite)
+    mySprite.setPosition(28, 70)
+    mySprite.ay = 350
+}
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorLockedEast, function (sprite, location) {
+    level += 1
+    levelstart()
+})
+let mySprite: Sprite = null
+let level = 0
+level = 0
+levelstart()
+game.onUpdate(function () {
+    mySprite.setImage(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `)
+    if (mySprite.vx < 0) {
+        mySprite.image.flipX()
+    }
+    if (mySprite.vy < 0) {
+        mySprite.setImage(img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `)
+    } else if (mySprite.vy > 0) {
+        mySprite.setImage(img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `)
+    } else {
+    	
+    }
+})
 forever(function () {
 	
 })
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,5 +3,75 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
 
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100003030303030303030303030303030303030405060405060405060405060406030306040506040506040506040606040303050604020202020202050604050603030402060405060405060202020405030306020206040506040506040506040303050202020202050604050604050603030405060405060405060405060605030306040506040506020206040506040303050604050604050604050604050601030405060405060205060405060402030306040506040506040506040506040303050604050202020204050604050603030405060405060405060405060405030306040502020202020202040506040303030303030303030303030303030303`, img`
+. 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . 2 2 2 2 2 2 . . . . . 2 
+2 . 2 . . . . . . . 2 2 2 . . 2 
+2 . 2 2 . . . . . . . . . . . 2 
+2 . 2 2 2 2 2 . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . 2 2 . . . . . 2 
+2 . . . . . . . . . . . . . . . 
+2 . . . . . . 2 . . . . . . 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . 2 2 2 2 . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . 2 2 2 2 2 2 2 . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.doorLockedEast,sprites.dungeon.floorLight0,sprites.dungeon.floorDark0,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.darkGroundNorthEast1], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`10001000020303030303030303030303030303060108080808080808080808080808080501080a0a0a0a0a080a0a080808080805010a0a080c0c0c0c0c0c090a09080805010a0c080a0a080a0a080c0c0c0a0805010a0c0c0a080a09090a0a0a0808080501080c0c0c0c0c090809080a0808080501080a09080a0a0a0a080a0a0a08080501080a09080a09080c0c08090a08080501080a09080a0809080908090a08080b01080a090808080c0808090908080c0501080809080808080a0a09080808080501080809090c0c0c0c090a0a0808080501080a0a0a0a0a0808080808080a0805010808080c0c0c0c0c0c0c0a0a0a0a050c0c0c0c040404040404040404040407`, img`
+. 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . 2 2 2 2 2 2 . . . . . 2 
+2 . 2 . . . . . . . 2 2 2 . . 2 
+2 . 2 2 . . . . . . . . . . . 2 
+2 . 2 2 2 2 2 . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . 2 2 . . . . . 2 
+2 . . . . . . . . . . . . . . . 
+2 . . . . . . 2 . . . . . . 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . 2 2 2 2 . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . 2 2 2 2 2 2 2 . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.darkGroundNorthEast1,sprites.dungeon.doorLockedEast,sprites.dungeon.floorLight0], TileScale.Sixteen);
+            case "level3":
+            case "level3":return tiles.createTilemap(hex`10001000020303030303030303030303030303060108080808080808080808080808080501080a0a0a0a0a080a0a080808080805010a0a080c0c0c0c0c0c090a09080805010a0c080a0a080a0a080c0c0c0a0805010a0c0c0a080a09090a0a0a0808080501080c0c0c0c0c090809080a0808080501080a09080a0a0a0a080a0a0a08080501080a09080a09080c0c08090a08080501080a09080a0809080908090a08080b01080a090808080c0808090908080c0501080809080808080a0a09080808080501080809090c0c0c0c090a0a0808080501080a0a0a0a0a0808080808080a0805010808080c0c0c0c0c0c0c0a0a0a0a050c0c0c0c040404040404040404040407`, img`
+. 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . 2 2 2 2 2 2 . . . . . 2 
+2 . 2 . . . . . . . 2 2 2 . . 2 
+2 . 2 2 . . . . . . . . . . . 2 
+2 . 2 2 2 2 2 . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . 2 2 . . . . . 2 
+2 . . . . . . . . . . . . . . . 
+2 . . . . . . 2 . . . . . . 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . 2 2 2 2 . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . 2 2 2 2 2 2 2 . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.darkGroundNorthEast1,sprites.dungeon.doorLockedEast,sprites.dungeon.floorLight0], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
 }
 // Auto-generated code. Do not edit.
